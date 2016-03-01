44843e58874c8aab04055eab330995c16e0826d8
M Haney <matthew.haney@utexas.edu>
2016-02-29 20:19:07 -0600

    Yay!

--- Homework3/src/Client.java
+++ Homework3/src/Client.java
@@ -1,10 +1,13 @@
-import java.util.Scanner;
+import java.util.*;
+import java.net.*;
+import java.io.*;
 
 public class Client {
   public static void main (String[] args) {
     String hostAddress;
     int tcpPort;
     int udpPort;
+    int udpBufSize = 1024;
 
     if (args.length != 3) {
       System.out.println("ERROR: Provide 3 arguments");
@@ -13,11 +16,31 @@ public class Client {
       System.out.println("\t(3) <udpPort>: the port number for UDP connection");
       System.exit(-1);
     }
-
+    
     hostAddress = args[0];
     tcpPort = Integer.parseInt(args[1]);
     udpPort = Integer.parseInt(args[2]);
 
+    InetAddress host;
+    Socket clientSocket;
+    DatagramSocket udpSocket;
+    
+    BufferedReader tcpIn;
+    BufferedWriter tcpOut;
+    DatagramPacket packetIn, packetOut;
+    byte[] buf = new byte[udpBufSize];
+    try {
+    	host = InetAddress.getByName(hostAddress);
+        clientSocket = new Socket(host, tcpPort);
+        udpSocket = new DatagramSocket(udpPort, host);
+        tcpIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        tcpOut = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+    
+    
     Scanner sc = new Scanner(System.in);
     while(sc.hasNextLine()) {
       String cmd = sc.nextLine();
@@ -26,6 +49,13 @@ public class Client {
       if (tokens[0].equals("purchase")) {
         // TODO: send appropriate command to the server and display the
         // appropriate responses form the server
+    	String username, productName, protocol;
+    	int quantity;
+    	username = tokens[1];
+    	productName = tokens[2];
+    	quantity = Integer.parseInt(tokens[3]);
+    	protocol = tokens[4];
+    	
       } else if (tokens[0].equals("cancel")) {
         // TODO: send appropriate command to the server and display the
         // appropriate responses form the server
--- Homework3/src/Server.java
+++ Homework3/src/Server.java
@@ -1,3 +1,9 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
+import java.net.InetAddress;
+
 public class Server {
   public static void main (String[] args) {
     int tcpPort;
@@ -10,9 +16,20 @@ public class Server {
 
       System.exit(-1);
     }
+    InetAddress localHost;
     tcpPort = Integer.parseInt(args[0]);
     udpPort = Integer.parseInt(args[1]);
     String fileName = args[2];
+    ServerSocket tcpSocket;
+    DatagramSocket udpSocket;
+    try {
+    	localHost = InetAddress.getLocalHost();
+		tcpSocket = new ServerSocket(tcpPort, 0, localHost);
+		udpSocket = new DatagramSocket(udpPort, localHost);
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 
     // parse the inventory file
 
